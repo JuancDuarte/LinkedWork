31d6a0c3ac623657f5e0500f27a59859a230ca11
JuancDuarte <juan.duarte09@uptc.edu.co>
2026-03-23 22:33:56 -0500

    Eliminar solicitud

--- src/main/java/com/contact/LinkedWork/controller/ServicioController.java
+++ src/main/java/com/contact/LinkedWork/controller/ServicioController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +38,11 @@ public class ServicioController {
     public List<Solicitud> getAllSolicitudes() {
         return solicitudService.getAllSolicitudes();
     } 
+@DeleteMapping(path = "/deleteSolicitud/{idSolicitud}/{idUsuario}")
+    public ResponseEntity<String> deleteSolicitudByUsuarioId(@PathVariable Long idSolicitud, @PathVariable Long idUsuario) {
+        solicitudService.deleteSolicitudByUsuarioId(idSolicitud, idUsuario);
+        return ResponseEntity.ok("Solicitud eliminada correctamente.");
+    }
 } 
 
 
