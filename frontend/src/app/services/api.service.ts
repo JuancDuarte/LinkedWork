2f5369101d9e0787b363fa1a0954a8ef1fcecc79
JuancDuarte <juan.duarte09@uptc.edu.co>
2026-05-20 18:08:18 -0500

    conectar render a vercel

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -4,7 +4,7 @@ import { Observable, catchError, retry, throwError, tap } from 'rxjs';
 
 import { environment } from '../../environments/environment';
 
-const API_BASE = environment.apiBaseUrl;
+const API_BASE = "https://linkedwork.onrender.com"
 
 export interface ApiUser {
   idUsuario?: number;
