11ce8dd9fa15a616ccef675827a992ce65e78efc
JuancDuarte <juan.duarte09@uptc.edu.co>
2026-05-22 13:08:32 -0500

    chat y funcionalidades

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -2,9 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, retry, throwError, tap } from 'rxjs';
 
-import { environment } from '../../environments/environment';
-
-const API_BASE = environment.apiBaseUrl;
+const API_BASE = "https://linkedwork.onrender.com/LinkedApi";
 
 export interface ApiUser {
   idUsuario?: number;
